f53c4b7cdfde4a07e0e47c90892e8788637730bc
diogobitten <diogobittenc@gmail.com>
2025-01-27 11:16:54 -0300

    remove cache control

--- api/github.js
+++ api/github.js
@@ -19,12 +19,6 @@ export default async function handler(req, res) {
     const response = await fetch(GITHUB_API_URL, { headers });
     const data = await response.json();
 
-    // Configure o cabeçalho Cache-Control para desativar o cache
-    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
-    res.setHeader('Surrogate-Control', 'no-store');
-
 
     // Retorne os dados da API do GitHub
     res.status(200).json(data);
